d99c642a5c44aa578d4d2c01808343ca9a8cc0b0
Julian Gutierrez <165670158+jmgtz01@users.noreply.github.com>
2025-09-17 18:21:48 +0000

    My first program

---
+++ homework_1/src/main.rs
@@ -0,0 +1,22 @@
+fun fahrenheit_to_celsius(f: f64) -> f64{
+    const FAHREN: int = 32;
+    //(32°F − 32) × 5/9
+    (FAHREN - f) x (5/9)
+
+}
+
+fun celsius_to_fahrenheit(c: f64) -> f64{
+    //(0°C × 9/5) + 32
+    return c * (9/4);
+}
+
+
+fun assignment1(){
+    // Temp converter
+    return 1;
+    
+}
+
+fn main() {
+    assignment1();
+}
